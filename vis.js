a771453af167fb7fdd6cfa737e22014fc293cbb1
pmmiller55 <78656312+pmmiller55@users.noreply.github.com>
2021-11-04 10:40:41 -0400

    Add files via upload

--- vis.js
+++ vis.js
@@ -1,37 +1,41 @@
 //Load in the datasets
-d3.csv("create_data_v2/westA_A.csv").then(function(A) {
-	d3.csv("create_data_v2/westA_kVA.csv").then(function(kVA) {
-		
-		//console.log(A)
-	
+d3.json("A.json").then(function(A) { //load in each dataset on after the other so they all function in one
+	d3.csv("timeA.csv").then(function(time) { //also load in each time file. The time files will be used to access the index values so the times match between datasets
+
+
+//Set the size and select the svg.
 	var width = 500
 	var height= 500	
 	var svg =  d3.select("#viz1")
 		.style("width", width)
-		.style("height", height)	
-		
+		.style("height", height)
 	
-	var nodes = Array.from(d3.group(A, d => d.node_id))
-	
-	//console.log(nodes[1][1][1].reading)
-	//console.log(Array.from(nodes[1][1], d => d.reading))
-
-//the link data set will need to be done with the index numbers. Doing it by name won't work.	
 	var link = [{ "source":  0,  "target":  1} ,  {"source":  0,  "target":  2}]	
+
+	var time = Array.from(time)
+	console.log(time[0])
+
+//This section controls the slider on the page. This will allow us to slide trough the dates and ideally this will update the visualization as you slide.
+	var slider = document.getElementById("dateslide")
+ 	var output = document.getElementById("date")
+	output.innerHTML = JSON.stringify(time[slider.value])
+
+//currently these functions work to pull the value from the slider.
+	slider.oninput = function() {
+		var current = this.value
+		let datee = JSON.stringify(time[current])
+		
+	output.innerHTML = datee
+
+	}
 	
-	var times = d3.group(A, d => d.time)
-	console.log(times)	
-//	console.log(times.forEach())
-
-
-    var layout = d3.forceSimulation(nodes)
+    var layout = d3.forceSimulation(A)
 		.force('center', d3.forceCenter(width / 2 , height / 2))
 		.force('collisions', d3.forceCollide(10))
 		.force('many', d3.forceManyBody())
 		.force('link', d3.forceLink(link).distance(50))
 		.on('tick', ticked) 
-
-	
+	 
 //   var color =  d3.scaleLinear().domain(nodes).range(red, blue, green, black) //TODO: create a color scale to map on the groups of the graph nodes
     
      var edges = svg.append("g")
@@ -43,25 +47,22 @@ d3.csv("create_data_v2/westA_A.csv").then(function(A) {
                   .attr("stroke-width", 1) 
      
     var node = svg.append("g")
-                  .selectAll("circle")
-                  .data(nodes)
-                  .enter()
-                  .append("circle")
- 				  .attr("fill", function(d) {
+                .selectAll("circle")
+                .data(A)
+                .enter()
+                .append("circle") 
+			//	.attr("fill",
+		/*		function(d) {
 					  var reading = Array.from(d[1], d => d.reading)
 					  console.log(d[1])
-					  console.log(d[1].indexOf({node_id: 'WAF1A', time: '9/10/2020 14:00', reading: '0'}))
-					  
-					  
-					  
-					  
+					  console.log(d[1].indexOf({node_id: 'WAF1A', time: '9/10/2020 14:00', reading: '0'}))  
 					  return "black"
 					  
-				  }) 
-              //    .attr("fill", function (d, group) {return color(group)} ) //TODO: set the appropriate color for each node depending on its group
-                  .attr("r", 7)
-	
-     function ticked(){
+				  })  */
+			//  .attr("fill", function (d, group) {return color(group)} ) //TODO: set the appropriate color for each node depending on its group
+				.attr("r", 7)
+		
+    function ticked(){
       node
 	    .attr('cx', d => d.x)
         .attr('cy', d => d.y)
@@ -72,5 +73,6 @@ d3.csv("create_data_v2/westA_A.csv").then(function(A) {
         .attr("x2", d => d.target.x)
         .attr("y2", d => d.target.y)
 		}
+		
 	})
-})
+}) 
